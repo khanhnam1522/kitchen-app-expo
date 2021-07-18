0bfe3a37cab5b629a1aca4f5f7a74b28fdf16560
khanhnam1522 <khanhnam1522@gmail.com>
2021-07-17 22:17:59 -0700

    remove MainScreen

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,6 +1,5 @@
 // Pages
 export { default as Login } from './Login';
-export { default as MainScreen } from './MainScreen';
 export { default as Landing } from './Landing';
 export { default as Register } from './Register';
 export { default as EmailSubmission } from './ForgotPassword/EmailSubmission';
